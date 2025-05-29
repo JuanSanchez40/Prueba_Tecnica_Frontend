fbf1522e3a7ca72ac8aff52a338c553da2ac2b42
Juan Sánchez <juanmsg34@outlook.es>
2025-05-28 21:29:26 -0700

    se agregan variables de entorno para firebaseConfig y protejer la apiKey

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 // Configuración de Firebase
 // Aqui agregamos el SDK de Firebase
 export const firebaseConfig = {
-  apiKey: "AIzaSyApNeOYdFnojDl1yCW07CRpLzqN-3_djZo",
-  authDomain: "comprasapp-a4e7a.firebaseapp.com",
-  projectId: "comprasapp-a4e7a",
-  storageBucket: "comprasapp-a4e7a.firebasestorage.app",
-  messagingSenderId: "299414505382",
-  appId: "1:299414505382:web:20714f339fd3a944e7b167",
-  databaseURL: "https://comprasapp-a4e7a.firebaseio.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
